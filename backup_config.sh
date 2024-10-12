85fc5b343e6a58db92898a5f9aeddf2ccc833940
Oliver Ernster <oliverjernster@hotmail.com>
2024-10-12 13:38:15 +0100

    Backup backup script

---
+++ backup_config.sh
@@ -0,0 +1,12 @@
+cd
+cd ~/VT350
+git pull
+cp -r ~/printer_data/config/*.* .
+rm ./config/printer-*.cfg
+cp ~/printer_data/database/moonraker-sql.db ./database/moonraker-sql.db
+git add .
+git commit -m "printer config backup"
+git push
+cd
+
+
