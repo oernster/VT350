f2b2556e20e3290a254a49158d61ffb4b75c4048
Oliver Ernster <oliverjernster@hotmail.com>
2024-11-12 23:56:30 +0000

    Update backup script to remove old IS data

--- backup_script/backup_config.sh
+++ backup_script/backup_config.sh
@@ -3,6 +3,11 @@ cd ~/VT350
 git pull
 cp -r ~/printer_data/config/*.* .
 rm ./printer-*.cfg
+rm ./crowsnest.conf.*
+rm ./printer_data/config/ShakeTune_results/vibrations/*.*
+rm ./printer_data/config/ShakeTune_results/input_shaper/*.*
+rm ./printer_data/config/ShakeTune_results/belts/*.*
+rm ./printer_data/config/ShakeTune_results/axes_map/*.*
 cp ~/printer_data/database/moonraker-sql.db ./database/moonraker-sql.db
 git add .
 git commit -m "printer config backup"
