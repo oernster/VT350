2d74cd0a2dd9c6c92fe78cc8e64f4d8d32f316a9
oernster <oliverjernster@hotmail.com>
2024-12-26 02:42:38 +0000

    Add backup_config.sh which has theme backup as well.

---
+++ theme/backup_config.sh
@@ -0,0 +1,20 @@
+cd
+cd ~/VT350
+git pull
+rm ~/printer_data/config/printer-*.cfg
+cp -r ~/printer_data/config/*.* .
+cp -r ~/printer_data/config/.theme/*.* theme
+rm ./printer-*.cfg
+rm ./crowsnest.conf.*
+rm ./printer_data/config/ShakeTune_results/vibrations/*.*
+rm ./printer_data/config/ShakeTune_results/input_shaper/*.*
+rm ./printer_data/config/ShakeTune_results/belts/*.*
+rm ./printer_data/config/ShakeTune_results/axes_map/*.*
+cp ~/printer_data/database/moonraker-sql.db ./database/moonraker-sql.db
+rm -rf ShakeTune_results
+git add .
+git commit -m "printer config backup"
+git push
+cd
+
+
